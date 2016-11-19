7b0004c5bb1acfdae582f4f9193cd8e8a15b2952
july_home <july_home@192.168.10.55>
2016-11-20 01:45:03 +0900

    console 입 출력및 h2 db에 인서트, 업데이트, 조회 추가

--- jpa/src/main/java/com/example/JpaApplication.java
+++ jpa/src/main/java/com/example/JpaApplication.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,28 +17,62 @@ public class JpaApplication implements CommandLineRunner {
 	}
 	
 	public void run(String... args) throws Exception {
-		Member m  = new Member();
-		m.setAge(20);
-		m.setName("sunny");
-		repository.save(m);
+		int select = 0;
 		
-		Member m2  = new Member();
-		m2.setAge(25);
-		m2.setName("jun");
-		repository.save(m2);
+		while(select != 9){
+			Scanner sc = new Scanner(System.in);
+			System.out.print("select number insert(1), update(2), exit(9)?");
+			select = sc.nextInt();
+			
+			if(select == 1){
+				insert();
+				
+			} else if(select == 2){
+				update();
+			}
+			
+			result();
+		}
 		
+		if(select == 9){
+			System.out.print("Exit!! \n");
+			System.exit(0);
+		}
+
+		
+	}
+
+	private void result() {
 		Iterable<Member> findAll = repository.findAll();
+		System.out.println("-------------------------");
+		System.out.println("|  id  |  name  |  age  |");
+		System.out.println("-------------------------");
 		for (Member member : findAll) {
-			System.out.println(member.getAge() + " - "+member.getName()+ " - "+ member.getId());
+			System.out.printf("|  %d  |  %s  |  %d  |\n", member.getId(), member.getName(), member.getAge());
+			System.out.println("-------------------------");
 		}
+	}
+
+	private void update() {
+		Scanner sc_update = new Scanner(System.in);
+		System.out.print("write id, name, age ?");
+		String[] temp = sc_update.nextLine().split(",");
 		
-		m.setAge(30);
+		Member m  = new Member();
+		m.setId(Long.parseLong(temp[0].trim()));
+		m.setName(temp[1].trim());
+		m.setAge(Integer.parseInt(temp[2].trim()));
 		repository.save(m);
-		
-		Iterable<Member> findAll_2 = repository.findAll();
-		for (Member member : findAll_2) {
-			System.out.println(member.getAge() + " - "+member.getName()+ " - "+ member.getId());
-		}
-		
+	}
+
+	private void insert() {
+		Scanner sc_insert = new Scanner(System.in);
+		System.out.print("write name, age ?");
+		String[] temp = sc_insert.nextLine().split(",");
+
+		Member m  = new Member();
+		m.setName(temp[0].trim());
+		m.setAge(Integer.parseInt(temp[1].trim()));
+		repository.save(m);
 	}
 }
