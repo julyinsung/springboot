818c6337378ded637411d1153c12baf0efe87e33
julyinsung <julyinsung@gmail.com>
2016-11-23 14:11:05 +0900

    init

---
+++ jpa_mvc/src/main/java/com/example/JpaMvcApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://spring.io/guides/gs/spring-boot/
+ * @author july
+ *
+ */
+@SpringBootApplication
+public class JpaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMvcApplication.class, args);
+	}
+}
