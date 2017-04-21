dd240bc581a7127cc6d8c5dce26ba0fd3be80538
julyinsung <julyinsung@gmail.com>
2017-04-21 10:25:20 +0900

    스케즐러를 추가한 소스

---
+++ spring-batch-sample-2/src/main/java/com/example/BatchConfiguration.java
@@ -0,0 +1,135 @@
+package com.example;
+
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * <pre>
+ * 스케줄 설정을 통한 반복 작업 수행 
+ *  
+ *  추가된 소스
+ *  - EnableScheduling 어노테이션
+ *  - perform() 메소드
+ *  - private JobLauncher jobLauncher
+ *  - private JobCompletionNotificationListener listener
+ *  	JobCompletionNotificationListener를 추가한 이유는  perform() 메소드에서 
+ *  	JobExecution execution = jobLauncher.run(importUserJob(listener), param);
+ *  	importUserJob(listener)를 사용하기 위함.
+ *  
+ *  참고: http://whereami80.tistory.com/10
+ *</pre>
+ *  
+ * @author july
+ */
+@Configuration
+@EnableBatchProcessing
+@EnableScheduling
+public class BatchConfiguration {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public DataSource dataSource;
+    
+    @Autowired
+    private JobLauncher jobLauncher;
+    
+    @Autowired
+    private JobCompletionNotificationListener listener;
+    
+    //@Scheduled(cron = "1 53/3 17 * * ?")
+    @Scheduled(fixedDelay = 5000) // 5초
+    public void perform() throws Exception {
+
+        System.out.println("Job Started at :" + new Date());
+
+        JobParameters param = new JobParametersBuilder().addString("JobID",
+                String.valueOf(System.currentTimeMillis())).toJobParameters();
+
+        JobExecution execution = jobLauncher.run(importUserJob(listener), param);
+
+        System.out.println("Job finished with status :" + execution.getStatus());
+    }
+    
+    
+    // tag::readerwriterprocessor[]
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+        reader.setResource(new ClassPathResource("sample-data.csv"));
+        reader.setLineMapper(new DefaultLineMapper<Person>() {{
+            setLineTokenizer(new DelimitedLineTokenizer() {{
+                setNames(new String[] { "firstName", "lastName" });
+            }});
+            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                setTargetType(Person.class);
+            }});
+        }});
+        return reader;
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer() {
+        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
+        writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+    // end::readerwriterprocessor[]
+
+    // tag::jobstep[]
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener) {
+        return jobBuilderFactory.get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Person, Person> chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+    // end::jobstep[]
+}
