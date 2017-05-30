87c35c788a51328fece74f78a5730df193408528
julyinsung <julyinsung@gmail.com>
2017-05-30 09:59:10 +0900

    head first 샘플소스

---
+++ restapi-sample/src/main/java/head/first/ooad/ch05/mandolins/Guitar.java
@@ -0,0 +1,8 @@
+package head.first.ooad.ch05.mandolins;
+
+public class Guitar extends Instrument {
+
+	public Guitar(String serialNumber, double price, GuitarSpec spec) {
+		super(serialNumber, price, spec);
+	}
+}
